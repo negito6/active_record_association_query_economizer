2bfe15ad304d72a384cfc0f28f642036027056be
toru.yagi <toru.yagi@crowdworks.co.jp>
2017-01-02 14:19:52 +0900

    add minitest files

---
+++ test/helper.rb
@@ -0,0 +1,27 @@
+require 'bundler/setup'
+require 'minitest/autorun'
+require 'minitest/spec'
+require 'active_record'
+require 'activerecord_preload_economizer'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+
+ActiveRecord::Schema.verbose = false
+ActiveRecord::Schema.define do
+  create_table :users do |t|
+    t.boolean :postable
+  end
+
+  create_table :posts do |t|
+    t.string :body
+    t.references :user
+  end
+end
+
+class User < ActiveRecord::Base
+  has_many :posts
+end
+
+class Post < ActiveRecord::Base
+  belongs_to :user
+end
---
+++ test/preload_filter_test.rb
@@ -0,0 +1,14 @@
+require 'helper'
+
+describe 'preload' do
+  before do
+    @user_postable     = User.create postable: true
+    @user_not_postable = User.create postable: false
+    2.times.map { |i| @user_postable.posts.build(body: "sample body #{i}").save }
+    @user_not_postable.posts.build(body: "sample body by not postable user").save
+  end
+
+  it 'got preloaded associations' do
+    assert User.where(id: @user_postable.id).preload(:posts).first.posts.size.must_equal 2
+  end
+end
