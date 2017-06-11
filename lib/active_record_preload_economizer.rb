a683d37c988bbe3e9f33c7d701ca38962d08ba4e
toru.yagi <toru.yagi@crowdworks.co.jp>
2017-06-11 12:15:02 +0900

    Support activerecord 5.x

--- lib/active_record_preload_economizer.rb
+++ lib/active_record_preload_economizer.rb
@@ -4,7 +4,7 @@ activerecord_version = ActiveRecord.version
 case activerecord_version
 when Gem::Requirement.create(['>= 4.2', '< 5'])
   require 'active_record_preload_economizer/rails4-2/associations'
-when Gem::Requirement.create(['>= 5.0'])
+when Gem::Requirement.create(['>= 5.0', '<= 5.1.1'])
   require 'active_record_preload_economizer/rails5/associations'
 else
   puts "active_record_preload_economizer doesn't yet do anything on ActiveRecord #{activerecord_version}"
--- lib/active_record_preload_economizer/rails5/associations.rb
+++ lib/active_record_preload_economizer/rails5/associations.rb
@@ -5,14 +5,34 @@ module ActiveRecord
   module Associations
     module Builder
       class Association #:nodoc:
+        VALID_OPTIONS += [:preload_if]
       end
     end
 
     class Preloader
       class Association #:nodoc:
+        def preload_filters
+          [options[:preload_if]].flatten.compact
+        end
+
+        def owners_filtered
+          unless defined?(@owners_filtered)
+            @owners_filtered = owners
+            preload_filters.each do |filter|
+              case filter
+              when Proc
+                @owners_filtered.select!(&filter)
+              when Symbol
+                @owners_filtered.select! { |record| record.send(filter) }
+              end
+            end
+          end
+          @owners_filtered
+        end
+
         def owner_keys
           unless defined?(@owner_keys)
-            @owner_keys = owners.map do |owner|
+            @owner_keys = owners_filtered.map do |owner|
               owner[owner_key_name]
             end
             @owner_keys.uniq!
